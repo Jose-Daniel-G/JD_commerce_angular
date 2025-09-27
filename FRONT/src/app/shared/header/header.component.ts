2f9b0ed2ddd098cd95ad354cd4705cdc8a1b6934
Jose Daniel <jose.grijalba04@gmail.com>
2025-09-26 21:25:00 -0500

    .

--- FRONT/src/app/shared/header/header.component.ts
+++ FRONT/src/app/shared/header/header.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '../../core/services/auth.service';
 import { NgIf } from '@angular/common';
-import { TranslateService } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule, NgIf],
+  imports: [RouterModule, NgIf, TranslateModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
